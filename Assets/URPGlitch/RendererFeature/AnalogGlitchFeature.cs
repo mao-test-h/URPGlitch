37b480f05c44de82f84c81cb18392785d5322ecf
mao <miymao78.d@gmail.com>
2022-07-17 21:07:10 +0900

    fix: Roughly modified to work.

--- Assets/URPGlitch/RendererFeature/AnalogGlitchFeature.cs
+++ Assets/URPGlitch/RendererFeature/AnalogGlitchFeature.cs
@@ -2,108 +2,111 @@
 //     https://github.com/keijiro/KinoGlitch.git
 //     Assets/Kino/Glitch/AnalogGlitch.cs
 
+using System;
+
 namespace UnityEngine.Rendering.Universal.Glitch
 {
     public sealed class AnalogGlitchFeature : ScriptableRendererFeature
     {
-        sealed class CustomRenderPass : ScriptableRenderPass
+        [SerializeField] Shader shader;
+        AnalogGlitchRenderPass _scriptablePass;
+
+        public override void Create()
         {
-            // CommandBufferPoolから取得するタイミング
-            const string FinalBlitPassTag = "Final Blit Pass";
+            _scriptablePass = new AnalogGlitchRenderPass(shader);
+        }
+
+        // Here you can inject one or multiple render passes in the renderer.
+        // This method is called when setting up the renderer once per-camera.
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(_scriptablePass);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _scriptablePass.Dispose();
+        }
+
+        sealed class AnalogGlitchRenderPass : ScriptableRenderPass, IDisposable
+        {
+            const string RenderPassName = "AnalogGlitch Render Pass";
 
             // Material Properties
-            static readonly int MainTexID = Shader.PropertyToID("_MainTex");
             static readonly int ScanLineJitterID = Shader.PropertyToID("_ScanLineJitter");
             static readonly int VerticalJumpID = Shader.PropertyToID("_VerticalJump");
             static readonly int HorizontalShakeID = Shader.PropertyToID("_HorizontalShake");
             static readonly int ColorDriftID = Shader.PropertyToID("_ColorDrift");
 
-            readonly AnalogGlitchFeature _glitchFeature;
-            readonly RenderTexture _mainTexture;
+            readonly ProfilingSampler _profilingSampler;
+            readonly Material _glitchMaterial;
+            readonly AnalogGlitchVolume _volume;
+
             float _verticalJumpTime;
 
-            Material GlitchMaterial => _glitchFeature.MaterialInstance;
-            float ScanLineJitter => _glitchFeature.ScanLineJitter;
-            float VerticalJump => _glitchFeature.VerticalJump;
-            float HorizontalShake => _glitchFeature.HorizontalShake;
-            float ColorDrift => _glitchFeature.ColorDrift;
+            bool isActive =>
+                _glitchMaterial != null &&
+                _volume != null &&
+                _volume.IsActive;
 
-            public CustomRenderPass(AnalogGlitchFeature glitchFeature)
+            public AnalogGlitchRenderPass(Shader shader)
             {
-                _glitchFeature = glitchFeature;
+                renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+                _profilingSampler = new ProfilingSampler(RenderPassName);
+                _glitchMaterial = CoreUtils.CreateEngineMaterial(shader);
 
-                _mainTexture = new RenderTexture(Screen.width, Screen.height, 0);
-                _mainTexture.hideFlags = HideFlags.DontSave;
+                var volumeStack = VolumeManager.instance.stack;
+                _volume = volumeStack.GetComponent<AnalogGlitchVolume>();
             }
 
+            public void Dispose()
+            {
+                CoreUtils.Destroy(_glitchMaterial);
+            }
+
+            // Here you can implement the rendering logic.
+            // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+            // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+            // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
             public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
             {
-                var material = GlitchMaterial;
-                if (material == null) return;
-
-                var cmd = CommandBufferPool.Get(FinalBlitPassTag);
-
-                // Copy & Set MainTex.
-                var camera = renderingData.cameraData.camera;
-                var activeTexture = camera.activeTexture;
-                cmd.Blit(activeTexture, _mainTexture);
-                material.SetTexture(MainTexID, _mainTexture);
-
-                // Calc Glitch.
+                var isPostProcessEnabled = renderingData.cameraData.postProcessEnabled;
+                var isSceneViewCamera = renderingData.cameraData.isSceneViewCamera;
+                if (!isActive || !isPostProcessEnabled || isSceneViewCamera)
                 {
-                    _verticalJumpTime += Time.deltaTime * VerticalJump * 11.3f;
-
-                    var sl_thresh = Mathf.Clamp01(1.0f - ScanLineJitter * 1.2f);
-                    var sl_disp = 0.002f + Mathf.Pow(ScanLineJitter, 3) * 0.05f;
-                    material.SetVector(ScanLineJitterID, new Vector2(sl_disp, sl_thresh));
-
-                    var vj = new Vector2(VerticalJump, _verticalJumpTime);
-                    material.SetVector(VerticalJumpID, vj);
-
-                    material.SetFloat(HorizontalShakeID, HorizontalShake * 0.2f);
-
-                    var cd = new Vector2(ColorDrift * 0.04f, Time.time * 606.11f);
-                    material.SetVector(ColorDriftID, cd);
-                }
-                cmd.Blit(_mainTexture, camera.activeTexture, material);
-
-                // Execute CmdBuff.
-                context.ExecuteCommandBuffer(cmd);
-                CommandBufferPool.Release(cmd);
-            }
-        }
-
-        [SerializeField] Material _material = default;
-        CustomRenderPass _scriptablePass;
-        Material _materialInstance;
-
-        public float ScanLineJitter { get; set; } = 0f;
-        public float VerticalJump { get; set; } = 0f;
-        public float HorizontalShake { get; set; } = 0f;
-        public float ColorDrift { get; set; } = 0f;
-
-        Material MaterialInstance
-        {
-            get
-            {
-                if (_materialInstance == null)
-                {
-                    _materialInstance = Instantiate(_material);
+                    return;
                 }
 
-                return _materialInstance;
+                var cmd = CommandBufferPool.Get(RenderPassName);
+                cmd.Clear();
+                using (new ProfilingScope(cmd, _profilingSampler))
+                {
+                    var source = renderingData.cameraData.renderer.cameraColorTarget;
+
+                    var scanLineJitter = _volume.scanLineJitter.value;
+                    var verticalJump = _volume.verticalJump.value;
+                    var horizontalShake = _volume.horizontalShake.value;
+                    var colorDrift = _volume.colorDrift.value;
+
+                    _verticalJumpTime += Time.deltaTime * verticalJump * 11.3f;
+
+                    var slThresh = Mathf.Clamp01(1.0f - scanLineJitter * 1.2f);
+                    var slDisp = 0.002f + Mathf.Pow(scanLineJitter, 3) * 0.05f;
+                    _glitchMaterial.SetVector(ScanLineJitterID, new Vector2(slDisp, slThresh));
+
+                    var vj = new Vector2(verticalJump, _verticalJumpTime);
+                    _glitchMaterial.SetVector(VerticalJumpID, vj);
+                    _glitchMaterial.SetFloat(HorizontalShakeID, horizontalShake * 0.2f);
+
+                    var cd = new Vector2(colorDrift * 0.04f, Time.time * 606.11f);
+                    _glitchMaterial.SetVector(ColorDriftID, cd);
+
+                    Blit(cmd, ref renderingData, _glitchMaterial);
+
+                    context.ExecuteCommandBuffer(cmd);
+                    CommandBufferPool.Release(cmd);
+                }
             }
         }
-
-        public override void Create()
-        {
-            _scriptablePass = new CustomRenderPass(this)
-            {
-                renderPassEvent = RenderPassEvent.AfterRendering + 1, // 必ず最後に実行されるように調整.
-            };
-        }
-
-        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-            => renderer.EnqueuePass(_scriptablePass);
     }
 }
